c201c44d8075c3a99d9726d88e0101561902fe23
empi91 <korpet.filip@gmail.com>
2023-11-23 19:02:07 +0100

    Filling squares with unique numbers work

--- Sudoku/main.py
+++ Sudoku/main.py
@@ -1,4 +1,4 @@
-import random, time
+import random
 
 board_data = [
     [" . ", " . ", " . "], [" . ", " . ", " . "], [" . ", " . ", " . "],
@@ -16,9 +16,9 @@ board_data = [
 def draw_board(board):
     long_row = "|"
     for i in range(27):
-        if i%9 == 0:
+        if i % 9 == 0:
             print("-------------------------------")
-        if i%3 == 2:
+        if i % 3 == 2:
             for j in range(3):
                 if j == 2:
                     long_row = long_row + board[i][j] + "|"
@@ -36,24 +36,41 @@ def draw_board(board):
     return 0
 
 
+def find_number(no_list, board, square, row, field):
+    while True:
+        number = random.randint(1, 9)
+        if number in no_list:
+            continue
+        else:
+            no_list.append(number)
+            board[square + row][field] = f" {number} "
+            break
+    return 0
+
+
 def fill_board(board):
     for square_no in range(9):
+        square_numbers = []
         if square_no < 3:
             for row in range(0, 7, 3):
                 for field in range(3):
-                    number = random.randint(1, 9)
-                    board[square_no + row][field] = f" {number} "
+                    find_number(square_numbers, board, square_no, row, field)
         elif 2 < square_no < 6:
             for row in range(6, 13, 3):
                 for field in range(3):
-                    number = random.randint(1, 9)
-                    board[square_no + row][field] = f" {number} "
+                    find_number(square_numbers, board, square_no, row, field)
         elif square_no > 5:
             for row in range(12, 19, 3):
                 for field in range(3):
-                    number = random.randint(1, 9)
-                    board[square_no + row][field] = f" {number} "
+                    find_number(square_numbers, board, square_no, row, field)
     return 0
+
+
+def check_board(board):
+    
+    return 0
+
 
 fill_board(board_data)
 draw_board(board_data)
+check_board(board_data)
